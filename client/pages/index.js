8a0b69da393789d38baa5d7f34deb1c8af82fe8a
adogbeji <baka_1993@yahoo.co.uk>
2024-10-08 17:05:19 +0100

    Create home page & install SASS

---
+++ client/pages/index.js
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+            <h1>Home Page</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default Home;
