052a7be8c1706c6fd39d045d19388dd8a46c93e2
adogbeji <baka_1993@yahoo.co.uk>
2024-10-14 16:03:54 +0100

    Display dummy image in home page & start adding CSS

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,14 @@
 import Nav from '../components/Nav';
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }) {
   // return <Component {...pageProps} />
   return (
     <>
+      <Head>
+        <link rel="stylesheet" href="/css/styles.css"/>
+      </Head>
+
       <Nav/>
       <Component {...pageProps} />
     </>
--- client/pages/index.js
+++ client/pages/index.js
@@ -2,6 +2,7 @@ const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <img src="/images/gal-1.jpeg" alt="Dummy Image"/>
     </div>
   );
 };
