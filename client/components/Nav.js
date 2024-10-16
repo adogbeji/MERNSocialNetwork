b488f9a685c15a7dd997b8d1e220c19a95bc3988
adogbeji <baka_1993@yahoo.co.uk>
2024-10-16 15:47:16 +0100

    Convert remaining a tags --> Link components in Nav component

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -7,59 +7,35 @@ const Nav = () => {
         {/* <!-- Navbar Start --> */}
         <nav className="topnav">
             {/* <!-- Logo --> */}
-            <a href="#" className="topnav__logo-link">
+            <Link href="#" className="topnav__logo-link">
                 {/* <img src="images/logo.png" alt="Website Logo" className="topnav__logo"> */}
-            </a>
-
-            
-            {/* <!-- Navigation Links + Icons + Profile Picture -->
-            <div className="topnav__link-icon-box"> */}
+            </Link>
                 
                 {/* <!-- Links --> */}
                 <div className="topnav__link-dot-dropdown-box">
                     <div className="topnav__link-box">
-                        {/* <Link href="/" className="topnav__link topnav__link--1">
-                            <a>Home</a>
-                        </Link>
-
-                        <Link href="#" className="topnav__link topnav__link--2">
-                            <a>Login</a>
-                        </Link>
-
-                        <Link href="#" className="topnav__link topnav__link--3">
-                            <a>Register</a>
-                        </Link> */}
-
                         <Link href="#" className="topnav__link topnav__link--1">Home</Link>
-                        <Link href="#" className="topnav__link topnav__link--2">Account</Link>
-                        <Link href="#" className="topnav__link topnav__link--3">Settings</Link>
-
-                        {/* <a href="#" className="topnav__link topnav__link--1">Booking</a> */}
-                        {/* <a href="#" className="topnav__link topnav__link--2">Account</a> */}
-                        {/* <a href="#" className="topnav__link topnav__link--3">Settings</a> */}
-                        {/* <a href="#" className="topnav__link topnav__link--4">Orders</a> */}
+                        <Link href="#" className="topnav__link topnav__link--2">Login</Link>
+                        <Link href="#" className="topnav__link topnav__link--3">Register</Link>
                     </div>
                     
                     {/* <!-- Responsive Dot Icon Dropdown (#2) --> */}
                     <div className="topnav__link-dot-dropdown-2">
-                        <span className="topnav__link-dot-icon-box-2" onclick="openTopnavLinkDotDropDownTwo()">
-                            {/* <svg className="topnav__link-dot-icon-2">
+                        {/* <span className="topnav__link-dot-icon-box-2" onclick="openTopnavLinkDotDropDownTwo()">
+                            <svg className="topnav__link-dot-icon-2">
                                 <use xlink:href="images/sprite.svg#icon-dots-three-horizontal"></use>
-                            </svg> */}
-                        </span>
+                            </svg>
+                        </span> */}
                         <div id="topNavLinkDotDropdown_2" className="topnav__link-dot_2-dropdown-content">
-                            <a href="#" className="topnav__link-dot_2-link topnav__link-dot_2-link--1">
-                                Booking
-                            </a>
-                            <a href="#" className="topnav__link-dot_2-link topnav__link-dot_2-link--2">
-                                Account
-                            </a>
-                            <a href="#" className="topnav__link-dot_2-link topnav__link-dot_2-link--3">
-                                Settings
-                            </a>
-                            <a href="#" className="topnav__link-dot_2-link topnav__link-dot_2-link--4">
-                                Orders
-                            </a>
+                            <Link href="#" className="topnav__link-dot_2-link topnav__link-dot_2-link--1">
+                                Home
+                            </Link>
+                            <Link href="#" className="topnav__link-dot_2-link topnav__link-dot_2-link--2">
+                                Login
+                            </Link>
+                            <Link href="#" className="topnav__link-dot_2-link topnav__link-dot_2-link--3">
+                                Register
+                            </Link>
                         </div>
                     </div>
             
@@ -73,9 +49,9 @@ const Nav = () => {
         <nav className="topnav-mobilenav">
 
             {/* <!-- Logo --> */}
-            <a href="#" className="topnav-mobilenav__logo-link">
+            <Link href="#" className="topnav-mobilenav__logo-link">
                 {/* <img src="images/logo.png" alt="Website Logo" className="topnav-mobilenav__logo"> */}
-            </a>
+            </Link>
 
             {/* <!-- Hamburger Icon --> */}
             <div className="topnav-mobilenav__bars-icon" onclick="openMobileMenu()">
@@ -89,10 +65,10 @@ const Nav = () => {
         <nav className="topnav-mobilenav__mobile-menu">
 
             <div className="topnav-mobilenav__link-box">
-                <a href="#" className="topnav-mobilenav__link">Bookng</a>
-                <a href="#" className="topnav-mobilenav__link">Account</a>
-                <a href="#" className="topnav-mobilenav__link">Settings</a>
-                <a href="#" className="topnav-mobilenav__link">Orders</a>
+                <Link href="#" className="topnav-mobilenav__link">Bookng</Link>
+                <Link href="#" className="topnav-mobilenav__link">Account</Link>
+                <Link href="#" className="topnav-mobilenav__link">Settings</Link>
+                <Link href="#" className="topnav-mobilenav__link">Orders</Link>
             </div>
         
         </nav>
