d611b5bd4df858245d8ad458d7c40a8aa776cb33
adogbeji <baka_1993@yahoo.co.uk>
2024-10-19 16:13:16 +0100

    Create SASS file for home page & import it

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,9 @@
+import styles from './styles/index.module.scss';
+
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1 className={styles.text}>Home Page</h1>
       <img src="/images/gal-1.jpeg" alt="Dummy Image"/>
     </div>
   );
