1343014eeb8e8fd1ef6a96cd9096ffc7c79eb7ff
adogbeji <baka_1993@yahoo.co.uk>
2024-10-26 16:03:10 +0100

    Define grid structure for register page

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,7 +1,11 @@
+import styles from './styles/register.module.scss';
+
 const Register = () => {
   return (
-    <div className="_0053">
-      <h1>Register Page</h1>
+    <div className={styles._0053}>
+      <section className={styles._0053_sect_1}>SECTION 1</section>
+      <section className={styles._0053_sect_2}>SECTION 2</section>
+      <section className={styles._0053_sect_3}>SECTION 3</section>
     </div>
   );
 };
