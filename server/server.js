cb58960c9f4b7056501231b0da543109837b1dd8
adogbeji <baka_1993@yahoo.co.uk>
2024-11-13 19:58:57 +0000

    Start building server

---
+++ server/server.js
@@ -0,0 +1,9 @@
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import cors from 'cors';
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
