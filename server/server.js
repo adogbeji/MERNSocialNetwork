0a4fb910ee11e016f3986cc30ad991d75f03d95d
adogbeji <baka_1993@yahoo.co.uk>
2024-12-04 15:19:29 +0000

    Import fs module in server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@
 // import mongoose from 'mongoose';
 // import cors from 'cors';
 
+// const fs = require('fs');
+const { readdirSync } = require('fs');
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
