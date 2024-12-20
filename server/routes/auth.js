ce53a24f5de602ae186b759fcada310ce213a292
adogbeji <baka_1993@yahoo.co.uk>
2024-12-20 14:20:37 +0000

    Put /register POST controller into separate file

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+import {register} from '../controllers/auth';
+
 router.post('/register', (req, res) => {
     console.log('REGISTER ENDPOINT => ', req.body);
 });
