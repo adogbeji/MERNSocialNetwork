91ea9b198cf6b90258080acfe7dac78c952dd201
adogbeji <baka_1993@yahoo.co.uk>
2025-01-08 14:11:11 +0000

    Start setting schema fields

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
 const { Schema } = require('mongoose');
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+});
