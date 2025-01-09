b3e9cca8fe53350b0e63188b88fedfd84b92b8a6
adogbeji <baka_1993@yahoo.co.uk>
2025-01-09 13:58:53 +0000

    Set more fields on schema in user.js

--- server/models/user.js
+++ server/models/user.js
@@ -7,4 +7,20 @@ const userSchema = new Schema({
         trim: true,
         required: true,
     },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 64,
+    },
 });
