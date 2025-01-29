b860b3d0937c171204a66e00439c88ee03cc30ac
adogbeji <baka_1993@yahoo.co.uk>
2025-01-29 14:14:43 +0000

    Start writing code to has password

--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -1,3 +1,7 @@
 const bcrypt = require('bcryptjs');
 
-export const hashPassword = (password) => {};
+export const hashPassword = (password) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.getSalt(12, );
+    });
+};
