10a1d2b8e1652cd73e1b8a7043a28e15cbae6e60
adogbeji <baka_1993@yahoo.co.uk>
2025-02-01 16:05:02 +0000

    Call genSalt() and hash() methods

--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -2,6 +2,16 @@ const bcrypt = require('bcryptjs');
 
 export const hashPassword = (password) => {
     return new Promise((resolve, reject) => {
-        bcrypt.getSalt(12, );
+        bcrypt.getSalt(12, (err, salt) => {
+            if (err) {
+                reject(err);
+            }
+            bcrypt.hash(password, salt, (err, hash) => {
+                if (err) {
+                    resolve(err);
+                }
+                resolve(hash);
+            });
+        });
     });
 };
