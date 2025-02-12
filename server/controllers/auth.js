db6a2cc4630ee33db69475a966cc956b55740e29
adogbeji <baka_1993@yahoo.co.uk>
2025-02-12 14:24:06 +0000

    Destructure incoming request data

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,5 +3,6 @@
 // };
 
 exports.register = (req, res) => {
-    console.log('REGISTER ENDPOINT => ', req.body);
+    // console.log('REGISTER ENDPOINT => ', req.body);
+    const { name, email, password, secret } = req.body;
 };
